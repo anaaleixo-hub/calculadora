e1d77e7cb116383e0b4a882c48df67728f5e2eb0
Ana <ana.aleixo@edu.senai.br>
2026-05-12 15:26:37 -0300

    Arquivo alterado , cabeçalho removido

--- calculdora.py
+++ calculdora.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Meu Primeiro Programa em Python
-Autor: [Seu Nome]
-Data: [Data de Hoje]
-Descrição: Programa básico para aprender GitHub
-"""
 
 # Importações
 import datetime
@@ -24,8 +18,8 @@ def boas_vindas():
     print("   Bem-vindo ao GitHub!\n")
     
     # 2. Informações pessoais
-    nome = "[Seu Nome]"
-    turma = "[Turma]"
+    nome = "Ana Laura Clabuchar Aleixo"
+    turma = "2A"
     
     print("2️⃣ Sobre mim:")
     print(f"   Nome: {nome}")
